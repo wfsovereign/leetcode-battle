0816e63488d44ef4842225fa1198ec26b4447d76
wfsovereign <wfsovereign@outlook.cm>
2018-07-16 22:09:03 +0800

    codewars: other method, use math formula

--- multiples-of-3-or-5/index.js
+++ multiples-of-3-or-5/index.js
@@ -23,3 +23,10 @@ function solution(number) {
 
 console.log(solution(16))
 
+// other clever method
+
+function solution(number) {
+  var n3 = Math.floor(--number / 3), n5 = Math.floor(number / 5), n15 = Math.floor(number / 15);
+  return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15 + 1)) / 2;
+}
+
