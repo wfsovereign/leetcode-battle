0020773c9ebdf3968d73f75b9730069b22a33799
wfsovereign <wfsovereign@outlook.cm>
2019-04-02 22:36:32 +0800

    leetcode: three sum , optimize

--- 3-sum/index.js
+++ 3-sum/index.js
@@ -3,52 +3,54 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-
-  nums = nums.sort((a, b) => (a - b))
   const length = nums.length
+  if (length < 3) {
+    return []
+  }
+  nums = nums.sort((a, b) => (a - b))
   let result = []
   for (let i = 0; i < length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue
+    }
+
     let next = i + 1
     let l = length - 1
+
     if (nums[i] <= 0) {
       while (next < l) {
+        if (next - 1 > i && nums[next] === nums[next - 1] && l < length - 1 && nums[l] === nums[l + 1]) {
+          next++
+          l--
+          continue
+        }
+
         let sum = nums[i] + nums[next] + nums[l]
         if (sum > 0) {
           l--
         } else if (sum < 0) {
           next++
         } else {
-          if (result.length === 0 ||
-            result.length > 0 && (result[result.length - 1][0] !== nums[i] || result[result.length - 1][1] !== nums[next])
-          ) {
-            result.push([nums[i], nums[next], nums[l]])
-          }
+          result.push([nums[i], nums[next], nums[l]])
           next++
           l--
-
-          while (next < l && nums[next] === nums[next - 1]) next++
-          while (next < l && nums[l] === nums[l + 1]) l--
         }
       }
     }
   }
-
-  return result.reduce((pre, cur, index) => {
-    if (index === 0 || !(pre.some(item => {
-      return item[0] === cur[0] && item[1] === cur[1] && item[2] === cur[2]
-    }))) {
-      pre.push(cur)
-      return pre
-    }
-    return pre
-  }, [])
+  return result
 }
 
 /*
 * Runtime: 536 ms, faster than 29.47% of JavaScript online submissions for 3Sum.
 Memory Usage: 47 MB, less than 43.64% of JavaScript online submissions for 3Sum.
-* */
 
+The second method:
+
+Runtime: 168 ms, faster than 90.77% of JavaScript online submissions for 3Sum.
+Memory Usage: 46.8 MB, less than 56.37% of JavaScript online submissions for 3Sum.
+
+* */
 
 module.exports = {
   threeSum,
