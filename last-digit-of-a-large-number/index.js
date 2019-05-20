853659a7ed46334023653dc29c77fedebed0781e
wfsovereign <wfsovereign@outlook.cm>
2019-05-20 15:34:53 +0800

    leetcode: last digit of a large number, another method

--- last-digit-of-a-large-number/index.js
+++ last-digit-of-a-large-number/index.js
@@ -1,19 +1,46 @@
-
 var lastDigit = function(str1, str2) {
-  if (str2.length === 1 && parseInt(str2) === 0) {
-    return 1
-  }
-  const lastDigitOfTarget = parseInt(str1.substr(str1.length - 1))
-
-  let lastDigitOfExponent = parseInt(str2.substr(str2.length - 1))
-  if (str2.length > 1) {
-    lastDigitOfExponent = 10 + lastDigitOfExponent
-  }
-
-  const res = (lastDigitOfTarget * Math.pow(lastDigitOfTarget, lastDigitOfExponent - 1 >= 0 ? lastDigitOfExponent - 1 : 0)).toString()
-  return parseInt(res.substr(res.length - 1))
+  return +!+str2 || Math.pow(str1.slice(-1) % 10, str2.slice(-2) % 4 || 4) % 10
 }
 
+
+
+/*
+*
+* const prd = {
+  '2': {
+    period: 4,
+    sequence: [2,4,8,6]
+  },
+  '3': {
+    period: 4,
+    sequence: [3,9,7,1]
+  },
+  '4': {
+    period: 2,
+    sequence: [4,6]
+  },
+  '7': {
+    period: 4,
+    sequence: [7,9,3,1]
+  },
+  '8': {
+    period: 4,
+    sequence: [8,4,2,6]
+  },
+  '9': {
+    period: 2,
+    sequence: [9,1]
+  }
+};
+* find the 0-9 regular, all the number exponent regular number is 4
+* */
+var lastDigit2 = function(str1, str2) {
+  let key = [0,0,0,0, 1,1,1,1, 6,2,4,8, 1,3,9,7, 6,4,6,4, 5,5,5,5, 6,6,6,6, 1,7,9,3, 6,8,4,2, 1,9,1,9]
+  let i = +str1.slice(-1)
+  let j = str2.slice(-2) % 4
+  if (str2 === '0') return 1
+  return key[4 * i + j]
+}
 
 module.exports = {
   lastDigit,
